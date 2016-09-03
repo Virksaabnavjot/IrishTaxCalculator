e0453989b49080ca06a6f089e2312d99ae2ff1c6
Virksaabnavjot <virksaabnavjot@gmail.com>
2016-09-03 19:43:34 +0100

    Adding Src Code

---
+++ src/eclipse/IrishTaxCalculator/gen/ie/nsv/irishtaxcalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ie.nsv.irishtaxcalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/eclipse/IrishTaxCalculator/src/ie/nsv/irishtaxcalculator/UserInput.java
@@ -0,0 +1,14 @@
+package ie.nsv.irishtaxcalculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class UserInput extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_input);
+	}
+
+}
